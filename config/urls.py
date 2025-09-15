814802f1ff103f8403cc4696dc191cbe6b8c2f85
Vitor Campos <vitor.campos@spassu.com.br>
2025-09-15 12:47:16 -0300

    chore: Adição de url de health check.  Adição de makefile

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
+
+def healthcheck(_request):
+    return JsonResponse({"status": "ok"})
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('relatorios.urls')),
+    path('', healthcheck, name='healthcheck'),
 ] 
