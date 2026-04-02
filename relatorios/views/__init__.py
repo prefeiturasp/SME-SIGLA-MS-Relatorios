d599d0dd910f387fde3f00dc8e3d0bd41ef9c11d
eudesgomesss <developer.eudes@gmail.com>
2026-04-02 15:23:53 -0300

    chore(145880): corrige roteamento de arquivos estáticos e swagger

--- relatorios/views/__init__.py
+++ relatorios/views/__init__.py
@@ -1,6 +1,5 @@
 from .relatorios import RelatorioViewSet
-from .swagger import SwaggerFromFileView
 from .parametrizacao import ParametrizacaoViewSet
 from .personalizacao import PersonalizacaoViewSet
 
-__all__ = ['RelatorioViewSet', 'SwaggerFromFileView', 'ParametrizacaoViewSet', 'PersonalizacaoViewSet']
+__all__ = ['RelatorioViewSet', 'ParametrizacaoViewSet', 'PersonalizacaoViewSet']
